ff69d4e565b9c0e6237b50e0065004a2d88ba393
jplataquin <jp.lataquin@gmail.com>
2022-01-05 12:37:31 +0800

    favicon

--- routes/web.php
+++ routes/web.php
@@ -105,23 +105,6 @@ Route::get('homepage/{filename}', function ($filename)
 });
 
 
-Route::get('favicon.ico', function ($filename)
-{
-    $path = storage_path('favicon.ico');
-
-    if (!File::exists($path)) {
-        abort(404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
-});
-
 
 Route::get('js/{filename}', function ($filename)
 {
