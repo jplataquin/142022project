fb1364acabba281a2477273eeaac1aa2dc4d6c93
jplataquin <jp.lataquin@gmail.com>
2022-01-14 00:18:58 +0800

    new domain link

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -37,7 +37,7 @@
     </head>
     <body>
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">Queen White</a>
+            <a class="navbar-brand" href="https://queenwhite.ph/">Queen White</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
